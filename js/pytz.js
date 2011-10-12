dd599a656d66f077c9617eff0e5c7a5580b4b9f9
Marcin Kaszynski <marcin@setjam.com>
2011-10-12 16:35:35 +0200

    Implemented localToDate for DstTzInfos, the easy cases (outside of jumps/overlaps).

--- js/pytz.js
+++ js/pytz.js
@@ -56,19 +56,118 @@ var Tz = {
             var idx = Math.max(0, this.getRightTransitionInfo(tsk));
             return Tz.utcToDict(new Date(tsk + this.transitionInfo[idx][0] * 1000));
         };
+
+        this.normalize = function (date) {
+            return this.fromDate(date);
+        };
+
+        // Based DstTzInfo.localize.
+        this.toDate = function (date_dict, is_dst) {
+            var dt = Tz.dictToUtc(date_dict, -this.utcOffset * 1000);
+            var dt_tsk = Tz.dateToUnixTsk(dt);
+            var i;
+            var day_delta = 24 * 60 * 60 * 1000;
+
+            possible_loc_dt = []
+
+            var deltas = [-day_delta, +day_delta];
+            for (i in deltas) {
+                var loc_dt = Tz.dictToUtc(date_dict, deltas[i]);
+                var idx = Math.max(0, this.getRightTransitionInfo(Tz.dateToUnixTsk(loc_dt)));
+                var inf = this.transitionInfo[idx];
+                
+                // inf[0] is utcoffset
+                loc_dt = new Date(dt_tsk - inf[0] * 1000);
+                if (Tz.areDateDictsEqual(this.fromDate(loc_dt), date_dict)) {
+                    possible_loc_dt.push(loc_dt);
+                }
+            }
+
+            // No nice sets in JS.  Implement set-like behavior
+            // knowing that the array has at most 2 elements.
+            if ((possible_loc_dt.length == 2)
+                && (Tz.areDateDictsEqual(possible_loc_dt[0], possible_loc_dt[1])))
+            {
+                possible_loc_dt.pop();
+            }
+
+            if (possible_loc_dt.length == 1) {
+                return possible_loc_dt[0];
+            }
+
+//            # If there are no possibly correct timezones, we are attempting
+//            # to convert a time that never happened - the time period jumped
+//            # during the start-of-DST transition period.
+//            if len(possible_loc_dt) == 0:
+//                # If we refuse to guess, raise an exception.
+//                if is_dst is None:
+//                    raise NonExistentTimeError(dt)
+//    
+//                # If we are forcing the pre-DST side of the DST transition, we
+//                # obtain the correct timezone by winding the clock forward a few
+//                # hours.
+//                elif is_dst:
+//                    return self.localize(
+//                        dt + timedelta(hours=6), is_dst=True) - timedelta(hours=6)
+//    
+//                # If we are forcing the post-DST side of the DST transition, we
+//                # obtain the correct timezone by winding the clock back.
+//                else:
+//                    return self.localize(
+//                        dt - timedelta(hours=6), is_dst=False) + timedelta(hours=6)
+//    
+//    
+//            # If we get this far, we have multiple possible timezones - this
+//            # is an ambiguous case occuring during the end-of-DST transition.
+//    
+//            # If told to be strict, raise an exception since we have an
+//            # ambiguous case
+//            if is_dst is None:
+//                raise AmbiguousTimeError(dt)
+//    
+//            # Filter out the possiblilities that don't match the requested
+//            # is_dst
+//            filtered_possible_loc_dt = [
+//                p for p in possible_loc_dt
+//                    if bool(p.tzinfo._dst) == is_dst
+//                ]
+//    
+//            # Hopefully we only have one possibility left. Return it.
+//            if len(filtered_possible_loc_dt) == 1:
+//                return filtered_possible_loc_dt[0]
+//    
+//            if len(filtered_possible_loc_dt) == 0:
+//                filtered_possible_loc_dt = list(possible_loc_dt)
+//    
+//            # If we get this far, we have in a wierd timezone transition
+//            # where the clocks have been wound back but is_dst is the same
+//            # in both (eg. Europe/Warsaw 1915 when they switched to CET).
+//            # At this point, we just have to guess unless we allow more
+//            # hints to be passed in (such as the UTC offset or abbreviation),
+//            # but that is just getting silly.
+//            #
+//            # Choose the earliest (by UTC) applicable timezone.
+//            sorting_keys = {}
+//            for local_dt in filtered_possible_loc_dt:
+//                key = local_dt.replace(tzinfo=None) - local_dt.tzinfo._utcoffset
+//                sorting_keys[key] = local_dt
+//            first_key = sorted(sorting_keys)[0]
+//            return sorting_keys[first_key]
+
+        };
     },
 
-    localToDate: function (tzname, date_dict) {
-        var tz = Tz.getTzinfo(tzname);
-        return tz.toDate(date_dict);
+    localToDate: function (tzname, date_dict, is_dst) {
+        var tz = Tz.getTzInfo(tzname);
+        return tz.toDate(date_dict, is_dst);
     },
 
     dateToLocal: function (tzname, date) {
-        var tz = Tz.getTzinfo(tzname);
+        var tz = Tz.getTzInfo(tzname);
         return tz.fromDate(date);
     },
 
-    getTzinfo: function (tzname) {
+    getTzInfo: function (tzname) {
         // TODO: raise an exception on missing tz
         return Tz.timezones[tzname];
     },
@@ -104,6 +203,16 @@ var Tz = {
                         date.getUTCMinutes(),
                         date.getUTCSeconds(),
                         date.getUTCMilliseconds());
+    },
+
+    areDateDictsEqual: function (date_dict_a, date_dict_b) {
+        return ((date_dict_a.year === date_dict_b.year)
+                && (date_dict_a.month === date_dict_b.month)
+                && (date_dict_a.date === date_dict_b.date)
+                && (date_dict_a.hours === date_dict_b.hours)
+                && (date_dict_a.minutes === date_dict_b.minutes)
+                && (date_dict_a.seconds === date_dict_b.seconds)
+                && (date_dict_a.milliseconds === date_dict_b.milliseconds));
     },
 };
 
