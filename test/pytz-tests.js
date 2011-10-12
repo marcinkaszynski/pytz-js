192b73ce07142845d9bbaa2d10539c1374c6f8c7
Marcin Kaszynski <marcin@setjam.com>
2011-10-12 15:13:48 +0200

    Bugfix: I thought Javascript kept months in the 1-12 range.

--- test/pytz-tests.js
+++ test/pytz-tests.js
@@ -6,8 +6,20 @@ pavlov.specify('pytz-js', function () {
     });
 
     describe('Tz.dateToLocal', function () {
-        it('Works well for a timezone with moving offset', function () {
-            // No tricky stuff like time change dates.
+        it('Works well for a timezone with moving offset, during DST', function () {
+            var date = Tz.dateToLocal('Europe/Warsaw',
+                                      new Date(Date.UTC(2011, 9, 12,
+                                                        2, 48, 20, 100)));
+            assert(date.year).equals(2011);
+            assert(date.month).equals(9);
+            assert(date.date).equals(12);
+            assert(date.hours).equals(4);
+            assert(date.minutes).equals(48);
+            assert(date.seconds).equals(20);
+            assert(date.milliseconds).equals(100);
+        });
+
+        it('Works well for a timezone with moving offset, out of DST', function () {
             var date = Tz.dateToLocal('Europe/Warsaw',
                                       new Date(Date.UTC(2011, 10, 12,
                                                         2, 48, 20, 100)));
@@ -22,10 +34,10 @@ pavlov.specify('pytz-js', function () {
 
         it('Works well for UTC', function () {
             var date = Tz.dateToLocal('UTC',
-                                      new Date(Date.UTC(2011, 10, 12,
+                                      new Date(Date.UTC(2011, 9, 12,
                                                         2, 48, 20, 100)));
             assert(date.year).equals(2011);
-            assert(date.month).equals(10);
+            assert(date.month).equals(9);
             assert(date.date).equals(12);
             assert(date.hours).equals(2);
             assert(date.minutes).equals(48);
@@ -35,10 +47,10 @@ pavlov.specify('pytz-js', function () {
 
         it('Works well for GMT+1', function () {
             var date = Tz.dateToLocal('Etc/GMT+1',
-                                      new Date(Date.UTC(2011, 10, 12,
+                                      new Date(Date.UTC(2011, 9, 12,
                                                         2, 48, 20, 100)));
             assert(date.year).equals(2011);
-            assert(date.month).equals(10);
+            assert(date.month).equals(9);
             assert(date.date).equals(12);
             assert(date.hours).equals(1);
             assert(date.minutes).equals(48);
